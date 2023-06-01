61fbc4491de5c1e0cc0a1f3749c6df85847b48fe
Fabricio Paranhos <fabricio@nethermind.io>
2023-06-01 14:29:09 -0300

    better run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,40 @@
-#!/bin/bash
+#!/bin/bash 
 
-# Generate runtime information directly from the solidity contract
-kevm solc-to-k ERC20.sol ERC20 --pyk --main-module ERC20-VERIFICATION --verbose > erc20-bin-runtime.k
+set -euxo pipefail 
+         
+build() { 
+    # Generate runtime information directly from the solidity contract
+    kevm solc-to-k ERC20.sol ERC20 --pyk --main-module ERC20-VERIFICATION --verbose > erc20-bin-runtime.k
+         
+    # Kompile everything
+    kevm kompile ${spec} --pyk --target haskell --syntax-module VERIFICATION --main-module VERIFICATION --output-definition ${definition} --verbose
+}         
+         
+prove() { 
+    # Prove it
+    kevm prove ${spec} --backend haskell --definition ${definition} --pyk --claim ${claim} --verbose --save-directory proofs "$@"
+}
 
-# Kompile everything
-kevm kompile erc20-spec.md --pyk --target haskell --syntax-module VERIFICATION --main-module VERIFICATION --output-definition erc20-spec/haskell --verbose
+view() {
+    # Prove it
+    kevm view-kcfg ${spec} --definition ${definition} --claim ${claim} --verbose --save-directory proofs "$@"
+}
 
-# Prove it
-kevm prove erc20-spec.md --backend haskell --definition erc20-spec/haskell --pyk --claim ERC20-SPEC.approve.success --verbose
+spec=erc20-spec.md
+definition=erc20-spec/haskell
+claim=ERC20-SPEC.approve.success
+
+# build
+# prove
+view
+
+# #!/bin/bash
+
+# # Generate runtime information directly from the solidity contract
+# kevm solc-to-k ERC20.sol ERC20 --pyk --main-module ERC20-VERIFICATION --verbose > erc20-bin-runtime.k
+
+# # Kompile everything
+# kevm kompile erc20-spec.md --pyk --target haskell --syntax-module VERIFICATION --main-module VERIFICATION --output-definition erc20-spec/haskell --verbose
+
+# # Prove it
+# kevm prove erc20-spec.md --backend haskell --definition erc20-spec/haskell --pyk --claim ERC20-SPEC.approve.success --verbose
